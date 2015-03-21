b771cb4f6f3db4638a317852462611dc27d81a7e
chenshus <chen4265@hotmail.com>
2015-03-21 15:52:37 +0200

    //

--- config/application.js
+++ config/application.js
@@ -59,18 +59,18 @@ App.app.use(function (req, res, next) {
 var bodyParser = require('body-parser');
 App.app.use(bodyParser.urlencoded({extended:true}));
 
-var methodOverride = require('method-override')
+var methodOverride = require('method-override');
 App.app.use(methodOverride(function (req,res){
     if(req.body && typeof req.body==='object' && '_method' in req.body){
-        var method = req.body._method
-        delete req.body._method
+        var method = req.body._method;
+        delete req.body._method;
         return method.toLowerCase()
     }
 }));
 
 
 App.require("config/routes/root")(App.app);
-App.require('config/core/database')(process.env.DATABASE_URL || 'mongodb://localhost/Otw_'+env);
+App.require('config/core/database')(process.env.DATABASE_URL || 'mongodb://otwadmin:otwadmin@ds041248.mongolab.com:41248/otw_development');
 
 
 
